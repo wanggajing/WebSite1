9193b86e92f42763226de3626b42f86dee9adf3e
Wu Ke <wke918@aucklanduni.ac.nz>
2015-02-25 10:40:46 +0800

    按键改变Label数值(hidden)，按键改变textbox数值和宽度

---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Label1.Text = "0";
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int temp = Convert.ToInt32(Label1.Text);
+        temp++;
+        Label1.Text = temp.ToString();
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int temp = Convert.ToInt32(TextBox1.Text);
+        temp++;
+        TextBox1.Text = temp.ToString();
+        TextBox1.Width = new Unit(TextBox1.Width.Value + 10);
+    }
+}
