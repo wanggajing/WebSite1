677c87ed3fbfd4eb55c67de6638527ed487355fa
Wu Ke <wke918@aucklanduni.ac.nz>
2015-02-26 16:11:49 +1300

    Session实现自增

---
+++ Session.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Session.aspx.cs" Inherits="Session" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="SetSession" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="GetSession" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="0"></asp:Label>
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Increase" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Session.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Session : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Session["value"] = "0";
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Session["first"] = DateTime.Now;
+        Session["second"] = 100;
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Button2.Text = Session["first"].ToString() + Session["second"];
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        int temp=Convert.ToInt32(Session["value"]);
+        temp++;
+        Session["value"]=temp;
+        Label1.Text = temp.ToString();
+    }
+}
