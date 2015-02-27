028b80c47a22e2d8a54568164190fe5c6cf6c2fa
Wu Ke <wke918@aucklanduni.ac.nz>
2015-02-27 17:26:00 +1300

    外网访问

---
+++ WebPrinciple/Example.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Example.aspx.cs" Inherits="WebPrinciple_Example" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="Button1" runat="server" Text="Delete" OnClientClick="return confirm('are you sure?')" OnClick="Button1_Click" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Alert from server" />
+        <br />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="AlertFromMessageBox" />
+        <br />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Place a File" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebPrinciple/Example.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebPrinciple_Example : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = "delete successfully " + DateTime.Now;
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Write("<script type='text/javascript'>alert('You clicked me');</script>");
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        System.Windows.Forms.MessageBox.Show("gagagaga!!!");
+        //用Cassini developer绑定公网IP地址，让其他人能访问
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        File.WriteAllText("c:/newFile.txt", "hahahahhahahaha");
+    }
+}
