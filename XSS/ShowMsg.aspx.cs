2813c928868ca4410405bd47f9ec33dd8236cc7c
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-02 10:02:20 +0800

    XSS attack

---
+++ XSS/ShowMsg.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class XSS_ShowMsg : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            Label1.Text = Request["msg"];
+        }
+    }
+}
