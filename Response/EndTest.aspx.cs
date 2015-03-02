a099c13374e74f2a275cdfcaf570e1f6d9b82f41
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-03 12:02:06 +1300

    Response对象

---
+++ Response/EndTest.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Response_EndTest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string q=Request["q"];
+        if (q == "1")
+        {
+            Response.Write("one");
+        }
+        else if (q == "2")
+        {
+            Response.End();
+        }
+        else if (string.IsNullOrEmpty(q))
+        {
+            Response.Write("please enter your question");
+        }
+        else
+        {
+            Response.Redirect("Handler.ashx");
+        }
+    }
+}
---
+++ Response/Handler.ashx
@@ -0,0 +1,28 @@
+﻿<%@ WebHandler Language="C#" Class="Handler" %>
+
+using System;
+using System.Web;
+
+public class Handler : IHttpHandler {
+    
+    public void ProcessRequest (HttpContext context) {
+        context.Response.ContentType = "text/plain";
+        for (int i = 0; i < 20; i++)
+        {
+            System.Threading.Thread.Sleep(500);
+            context.Response.Write("The " + i + "thread has finished execution <br/>");
+           // context.Response.Flush();
+            if (i == 10)
+            {
+                context.Response.Clear();
+            }
+        }
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
