5a0daf49ac4da05b14cf1633cd4079fe25221bdd
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-03 10:49:45 +1300

    绝对路劲和相对路劲

---
+++ ZhongJi/Root.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Root.aspx.cs" Inherits="ZhongJi_Root" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <a href="/sdfsdf">hhh</a>
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/XSS/ShowMsg.aspx">HyperLink</asp:HyperLink>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ZhongJi/Root.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ZhongJi_Root : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("<a href='" + VirtualPathUtility.ToAbsolute("~/a/b.html") + "'>ggggg</a>");
+        Response.Write(Request.AppRelativeCurrentExecutionFilePath);
+        Response.Write(Request.PhysicalPath);
+    }
+}
